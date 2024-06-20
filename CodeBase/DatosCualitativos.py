b591ff4e4e5b8e97e57482f3e32b3eb7d77f18fc
Edwin Salvador Soto Hernandez <230110358@itsoeh.edu.mx>
2024-06-20 14:09:31 -0600

    Actualización de Datos Cualitativos

--- CodeBase/DatosCualitativos.py
+++ CodeBase/DatosCualitativos.py
@@ -1,4 +1,38 @@
-# TABLA HTML
+# OBTENER DATOS PARA LA TABLA DE FRECUENCIAS
+def buildFreqDist(lstDatos):
+    lstDatos.sort()
+    clase, frecAbs = [], []
+    for element in lstDatos:
+        if(element not in clase):
+            clase.append(element)
+            frecAbs.append(1)
+        else:
+            frecAbs[clase.index(element)] += 1
+        
+    frecAbsAc, frecRel, frecRelAc = [], [], []
+    frecAbsT = sum(frecAbs)
+    ultFa = 0
+    ultFr = 0
+    for fa in frecAbs:
+        fr = 100 / frecAbsT * fa
+        frecRel.append(fr)
+        frecRelAc.append(fr+ultFr)
+        frecAbsAc.append(fa+ultFa)
+        ultFr += fr
+        ultFa += fa
+        
+    return [clase, frecAbs, frecAbsAc, frecRel, frecRelAc]
+
+# FORMATEAR DATOS
+def formatStr(strArray):
+    strArraySorted = []
+    for element in strArray:
+        element = element.strip()
+        element = element.lower()
+        strArraySorted.append(element)
+    return strArraySorted;
+
+# IMPRIMIR TABLA
 from IPython.display import HTML, display
 def printHTMLTable(encabezados, contenido):
     if len(encabezados) == len(contenido):
@@ -19,52 +53,8 @@ def printHTMLTable(encabezados, contenido):
     else:
         print("Verificar longitud de encabezados y contenido")
 
+# MÉTODO PARA LLAMAR TABLA
+def mostrarTablaDeFrecuencias(data):
+    clases, fa, faAc, fr, frAc = buildFreqDist(data)
+    printHTMLTable(["Clase", "Fa", "Fr", "Fr Ac"], [clases, fa, fr, frAc])
 
-# Frecuencia absoluta
-def frecAbs(lstDatos):
-    lstDatos.sort()
-    clase, frecAbs = [], []
-    for element in lstDatos:
-        if(element not in clase):
-            clase.append(element)
-            frecAbs.append(1)
-        else:
-            frecAbs[clase.index(element)] += 1
-    return clase, frecAbs
-
-# Frecuencia Relativa
-def frecRel(frecAbs):
-    frecRel = []
-    frecAbsT = sum(frecAbs)
-    for element in frecAbs:
-        fr = 100 / frecAbsT * element
-        frecRel.append(fr)
-    return frecRel
-
-# Frecuencia Acumulada
-def frecAc(frec):
-    frecAc = []
-    ultVal = 0
-    for element in frec:
-        fAc = element
-        frecAc.append(fAc+ultVal)
-        ultVal += fAc
-    return frecAc
-
-def formatStr(strArray):
-    strArraySorted = []
-    for element in strArray:
-        element = element.strip()
-        element = element.lower()
-        strArraySorted.append(element)
-    return strArraySorted;
-
-#def clasesStrSort():
-#    a=1
-
-def mostrarTabla(datos):
-    datos2 = formatStr(datos)
-    clases, fa = frecAbs(datos2)
-    fr = frecRel(fa)
-    frAc = frecAc(fr)
-    printHTMLTable(["Clase",  "Fr", "Fr Ac"], [clases, fr, frAc])
